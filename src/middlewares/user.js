7f9b3c2894ab13d0660ee5bb225c39b8e14428fb
WiseyXD <aryan.s.nag@gmail.com>
2023-12-27 20:23:39 +0530

    feat : Working on Middleware

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const jwtKey = prooces.env.JWT_KEY;
+
 async function userMiddleware(req, res, next) {}
 
 module.exports = userMiddleware;
