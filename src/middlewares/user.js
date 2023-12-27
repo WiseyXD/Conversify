b631828b28b4adab2f0d825a001e3025cf76ea4e
WiseyXD <aryan.s.nag@gmail.com>
2023-12-27 23:10:03 +0530

    feat : User Middleware Done

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,7 +1,27 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const jwtKey = prooces.env.JWT_KEY;
+const jwtKey = process.env.JWT_KEY;
 
-async function userMiddleware(req, res, next) {}
+async function userMiddleware(req, res, next) {
+    try {
+        const headers = req.headers.authorization;
+        const token = headers && headers.split(" ")[1];
+        if (!token) {
+            throw new Error("No JWT Token provided");
+        }
+        jwt.verify(token, jwtKey, (err, decoded) => {
+            if (err) {
+                throw new Error("Invalid JWT Token");
+            }
+            req.email = decoded.email;
+            next();
+        });
+    } catch (error) {
+        const msg = error.message;
+        res.status(400).json({
+            msg,
+        });
+    }
+}
 
 module.exports = userMiddleware;
