15fb785f22eb3dcaf65f9ab62176d301d962abcd
WiseyXD <aryan.s.nag@gmail.com>
2023-12-30 13:42:43 +0530

    style : Tailwind and DaisyUI setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,24 @@ import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
     const [count, setCount] = useState(0);
     const navigate = useNavigate();
 
     return (
-        <Routes>
-            <Route path="/" element={<Chat />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<Error />} />
-        </Routes>
+        <>
+            <Navbar />
+            <div className="max-w-full mx-auto px-7">
+                <Routes>
+                    <Route path="/" element={<Chat />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </div>
+        </>
     );
 }
 
