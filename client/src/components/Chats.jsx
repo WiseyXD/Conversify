423a60f8bd5ab7c969e2740a525f82a3b323c0b7
WiseyXD <aryan.s.nag@gmail.com>
2023-12-31 19:29:00 +0530

    style : Search ba and Modal

--- client/src/components/Chats.jsx
+++ client/src/components/Chats.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ChatCard from "./ChatCard";
 import { useGetChatsByUserIdQuery } from "../redux/services/chatApi";
 import { useSelector } from "react-redux";
+import { MdAddComment } from "react-icons/md";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 export default function Chats() {
     const id = useSelector((state) => state.root.auth.id);
@@ -10,11 +12,52 @@ export default function Chats() {
         return null;
     }
     const { allChats } = data;
+    console.log(allChats);
     return (
         <div className="basis-1/3 min-h-screen flex flex-col gap-5 overflow-y-auto scrollbar-hide">
-            {allChats.map((chat) => (
-                <ChatCard chat={chat} />
-            ))}
+            <div className="flex justify-between items-center">
+                <div className="flex basis-10/12">
+                    <input
+                        type="text"
+                        placeholder="Find Chat here"
+                        className="w-full pl-2"
+                    />
+                    <button className="btn">
+                        <HiMagnifyingGlass size={35} />
+                    </button>
+                </div>
+                <div
+                    className="basis-2/12 flex justify-end tooltip-bottom"
+                    data-tip="CreateChat"
+                >
+                    <button
+                        className="btn"
+                        onClick={() =>
+                            document.getElementById("my_modal_3").showModal()
+                        }
+                    >
+                        <MdAddComment size={35} />
+                    </button>
+                    <dialog id="my_modal_3" className="modal">
+                        <div className="modal-box">
+                            <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                    ✕
+                                </button>
+                            </form>
+                            <h3 className="font-bold text-lg">
+                                Create New Chats!
+                            </h3>
+                            <p className="py-4">Map all the users</p>
+                        </div>
+                    </dialog>
+                </div>
+            </div>
+            <div className="flex flex-col">
+                {allChats.map((chat) => (
+                    <ChatCard chat={chat} />
+                ))}
+            </div>
         </div>
     );
 }
